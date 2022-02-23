666aee34ec322dbebd4822ac0cc6a46993a35b81
San0077 <99713211+San0077@users.noreply.github.com>
2022-02-23 09:39:47 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+var req = new XMLHttpRequest()
+req.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+req.send();
+req.onload=function(){
+    var data = JSON.parse(req.response)
+   for(var i in data){
+       console.log(`flag : ${data[i].flag}`)
+   }
+}
+
